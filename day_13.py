47ec6a8de6881c2bba816ba3f56256a0c8b40562
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-03-29 11:06:31 +0530

    Add files via upload

---
+++ day_13.py
@@ -0,0 +1,90 @@
+# print(len("akhila"))
+
+a="akhila"
+# print(a,type(a))
+# print(a[0])
+
+# print("akhila"[0])
+# print("akhila"[1])
+# print("akhila"[2])
+# print("akhila"[3])
+# print("akhila"[4])
+# print("akhila"[5])
+# print("akhila"[6])
+
+# print("computer"[0])
+# print("computer"[1])
+# print("computer"[2])
+# print("computer"[3])
+# print("computer"[4])
+# print("computer"[5])
+# print("computer"[6])
+# print("computer"[7])
+# print("computer"[8])
+
+a="department"
+# print(a[3]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9])
+# print(a[3]+a[2:10])
+# print(a[0]+a[4]+a[1]+a[3]+a[6])
+# print(a[0]+a[1]+a[3]+a[4])
+
+c="akhila"
+# print(c[:])
+# print(c[0:6])
+# print(c[0:0])
+# print(c[0:1])
+# print(c[0:2])
+# print(c[0:3])
+# print(c[0:4])
+# print(c[0:5])
+# print(c[0:6])
+# print(c[0:7])
+# print(c[0:8])
+
+d="hello"
+# print(d[:])
+# print(d[0:5])
+# print(d[0:0])
+# print(d[0:1])
+# print(d[0:2])
+# print(d[0:3])
+# print(d[0:4])
+# print(d[0:5])
+# print(d[0:6])
+# print(d[3:2])
+# print(d[5:3])
+# print(d[7:6])
+# print(d[0:-1])
+# print(d[0:-2])
+# print(d[0:-3])
+# print(d[0:-4])
+# print(d[0:-5])
+
+k="reddy"
+# print(k[2:4])
+# print(k[1:5])
+# print(k[3:5])
+
+h="akhila"
+# print(h[-1:-6])
+# print(h[-2:-5])
+# print(h[-5:-3])
+# print(h[-6:-1])
+
+a="akhila"
+# print("akhila"[-1])
+# print("akhila"[-2])
+# print("akhila"[-3])
+# print("akhila"[-4])
+# print("akhila"[-5])
+# print("akhila"[-6])
+
+a="akhila"
+# print(a[ : : ])
+# print(a[0 : : 1])
+# print(a[ : : 2])
+# print(a[ : : 3])
+# print(a[ : : 4])
+# print(a[ : : 5])
+# print(a[ : : 6])
+# print(a[ : : 7])
